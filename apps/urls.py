8b7b494768f6307c31eed3bb3f85b2457097cf09
Rustam <rustamjonisroilov213@gmail.com>
2023-11-12 16:29:45 +0500

    first commit

---
+++ apps/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path, include
+
+urlpatterns = [
+    path('', include('blog.urls')),
+    # path('about/', include('about.urls')),
+    # path('contact/', include('contact.urls')),
+]
